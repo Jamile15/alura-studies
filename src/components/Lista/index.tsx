fff2275f916f32249b0145c0fa9146e02ac7e1c7
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-03-28 10:53:32 -0300

    criando uma lista

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Lista() {
+    return (
+        <aside>
+            <h2>Estudps do dia</h2>
+            <ul>
+                <li>
+                    <h3>React</h3>
+                    <span>02:00:00</span>
+
+                </li>
+                <li>
+                    <h3>Javascript</h3>
+                    <span>01:00:00</span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
