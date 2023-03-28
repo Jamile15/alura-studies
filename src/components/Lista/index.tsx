73dbd7c108e45ef9caa143e16a894e3d34daa5ba
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-03-28 11:34:15 -0300

    Renderizando um array

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 
 function Lista() {
-    return (
-        <aside>
-            <h2>Estudps do dia</h2>
-            <ul>
-                <li>
-                    <h3>React</h3>
-                    <span>02:00:00</span>
-
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '01:00:00'
+    }, {
+        tarefa: 'javascript',
+        tempo: '01:00:00'
+    
+    },{
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }]
+return (
+    <aside>
+        <h2>Estudos do dia</h2>
+        <ul>
+            {tarefas.map((item, index) =>(
+                <li key={index}>
+                    <h3>{item.tarefa}</h3>
+                    <span>{item.tempo}</span>
                 </li>
-                <li>
-                    <h3>Javascript</h3>
-                    <span>01:00:00</span>
-                </li>
-            </ul>
-        </aside>
-    )
+            ))}
+            
+        </ul>
+    </aside>
+)
 }
 
 export default Lista;
