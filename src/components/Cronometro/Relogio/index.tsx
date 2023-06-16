8fef5c0b8f25138d381879a3cd211732e64bcb6d
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-06-16 09:42:52 -0300

    pegando o valor do select pro cronometro

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,21 @@
+import { match } from 'assert'
 import Style from './relogio.module.scss'
+interface Props{
+    tempo: number | undefined
+}
 
-export default function Relogio() {
+export default function Relogio({tempo = 0} : Props) {
+    const minutos = Math.floor(tempo /60);
+    const segundos = tempo % 60;
+    const  [minutoDezenna, minutoUnidade] = String(minutos). padStart(2, '0');
+    const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, '0');
     return (
         <>
-            <span className={Style.relogioNumero}>0</span>
-            <span className={Style.relogioNumero}>0</span>
+            <span className={Style.relogioNumero}>{minutoDezenna}</span>
+            <span className={Style.relogioNumero}>{minutoUnidade}</span>
             <span className={Style.relogioDivisao}>:</span>
-            <span className={Style.relogioNumero}>0</span>
-            <span className={Style.relogioNumero}>0</span>
+            <span className={Style.relogioNumero}>{segundoDezena}</span>
+            <span className={Style.relogioNumero}>{segundoUnidade}</span>
         </>
     )
 }
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,16 +1,31 @@
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './cronometro.module.scss';
+import React, { useEffect, useState } from 'react';
 
-export default function Cronometro() {
-    console.log('conversao: ', tempoParaSegundos ('01:01:01'))
-    return (
+
+interface Props {
+    selecionado : ITarefa | undefined
+}
+
+export default function Cronometro({selecionado} :  Props) {
+
+    const [tempo, setTempo] = useState<number>();
+    useEffect(() => {
+        if(selecionado?.tempo) {
+          setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+      },[selecionado])
+
+      
+      return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
             <div className={style.relogioWrapper}>
 
-                <Relogio />
+                <Relogio tempo={tempo} />
 
             </div>
             <Botao type="submit">
